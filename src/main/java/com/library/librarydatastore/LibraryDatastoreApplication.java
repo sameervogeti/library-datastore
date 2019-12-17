1d0d5e3409c401ef365b0af0eef0e2156b1693b0
svogeti <sameervogeti@gmail.com>
2019-12-17 14:04:20 +0530

    Initial Commit

---
+++ src/main/java/com/library/librarydatastore/LibraryDatastoreApplication.java
@@ -0,0 +1,13 @@
+package com.library.librarydatastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryDatastoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryDatastoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/library/librarydatastore/LibraryDatastoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.librarydatastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryDatastoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
